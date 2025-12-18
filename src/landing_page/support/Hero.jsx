e733439f02b8e4a185e564676e3b6de10cbd0df3
Yug Patel <yug35287@gmail.com>
2025-12-18 20:12:57 +0530

    Support page done

--- src/landing_page/support/Hero.jsx
+++ src/landing_page/support/Hero.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return (
-        <h1>Hero</h1>
-    );
+  return (
+    <section className="container-fluid" id="supportHero">
+      
+      <div className="p-5" id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="" className="fs-5">Track Tickets</a>
+      </div>
+
+      
+      <div className="row p-5" id="contentWrapper">
+        <div className="col-7 px-5">
+          <h1 className="fs-3 mb-4">
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+          <div className="search-container mb-4">
+            <input placeholder="Eg: how do I activate F&O, help with account" />
+          </div>
+          <div className="hero-links">
+            <a href="">Track account opening</a>
+            <a href="">Track segment activation</a>
+            <a href="">Intraday margins</a>
+            <a href="">Kite user manual</a>
+          </div>
+        </div>
+        
+        <div className="col-5 px-5">
+          <h1 className="fs-3 mb-4">Featured</h1>
+          <ol>
+            <li className="mb-3">
+              <a href="">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
--- src/landing_page/support/SupportPage.jsx
+++ src/landing_page/support/SupportPage.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import CreateTicket from './CreateTicket';
-import Hero from './Hero';
+import React from "react";
+
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
+
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 function SupportPage() {
-    return (
-        <>
-            <Hero/>
-            <CreateTicket/>      
-        </>
-    );
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
 export default SupportPage;
